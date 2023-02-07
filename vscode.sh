a7daa270f20f957321d2727387ab80e166c64747
0mkar01 <omkar.alcatraz@gmail.com>
2023-02-07 01:43:27 -0500

    Second commit

--- vscode.sh
+++ vscode.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-if [[ ${UID} == 0 ]]
-then
-    echo 'Please login as a super user other than root !'
-else    
-    $(sudo echo -e '[code]
-    name=Visual Studio Code 
-    baseurl=https://packages.microsoft.com/yumrepos/vscode 
-    enabled=1 
-    gpgcheck=1 
-    gpgkey=https://packages.microsoft.com/keys/microsoft.asc' > /etc/yum.repos.d/vscode.repo)
-    $(sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc)
-    $(sudo yum install code)
-fi
+$(sudo echo -e '[code]
+name=Visual Studio Code 
+baseurl=https://packages.microsoft.com/yumrepos/vscode 
+enabled=1 
+gpgcheck=1 
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc' > /etc/yum.repos.d/vscode.repo)
+$(sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc)
+$(sudo yum install code)
