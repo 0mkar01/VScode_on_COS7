b32bb960eccd5aeaa42be2ec52c7778e3be3cfa6
X <X@master.sniper.com>
2023-02-07 16:13:30 +0530

    Third commit

--- vscode.sh
+++ vscode.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-$(sudo echo -e '[code]
+echo -e '[code]
 name=Visual Studio Code 
 baseurl=https://packages.microsoft.com/yumrepos/vscode 
 enabled=1 
 gpgcheck=1 
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc' > /etc/yum.repos.d/vscode.repo)
-$(sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc)
-$(sudo yum install code)
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc' | sudo tee /etc/yum.repos.d/vscode.repo
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo yum install code
